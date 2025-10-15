f35164967ba4fc56bf27fa016057accebb2bee06
kakitetsu0310-dev <kakitetsu0310@gmail.com>
2025-10-15 11:22:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.getElementById('bookingForm').addEventListener('submit', function(event
     const messageDiv = document.getElementById('message');
 
     // ★★★ ここにあなたのGoogle Apps ScriptのURLを貼り付けます ★★★
-    const gasUrl = 'https://script.google.com/macros/s/AKfycbxQ5PSf9DA8ATWVZzlZw_9b-MQWrNn_Em3gltSkzR36tL9asjz0qCjQlMEYNPfqpM8R/exec';
+    const gasUrl = 'https://script.google.com/macros/s/AKfycbxihDlvMnVWNpUfIkmdj5Q1OvV71MVb8xOb3zS-JZQaFRKdkOF3PN3KQwuM4XfMloRU/exec';
 
     // 予約内容の確認メッセージを準備
     const bookingSummary = `
